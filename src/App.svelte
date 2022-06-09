7be04fdec279b839eb47b7c301a071321ab1666f
Ali Barış Öztürk <alibarisozturk@gmail.com>
2022-06-09 17:59:17 +0300

    toparlamaya basladim

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,6 @@
 <script>
-  //import logo from './assets/svelte.png'
-  import { useRegisterSW } from "virtual:pwa-register/svelte";
-  import { onMount } from "svelte/internal";
-  import Board from './Board.svelte'
-  const { updateServiceWorker } = useRegisterSW();
-  onMount(()=>{
-    updateServiceWorker();
-  });
+  //import logo from './assets/svelte.png'  
+  import Board from './Board.svelte';
 </script>
 
 <main>
--- vite.config.js
+++ vite.config.js
@@ -1,55 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { VitePWA } from 'vite-plugin-pwa';
+import glob from 'glob';
+import path from 'path';
+
+const pages = glob.sync('src/**.js').reduce(function (obj, el) {
+  obj[path.parse(el).name] = el;
+  return obj
+}, {});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), VitePWA({
-    strategies: "injectManifest",
-    mode: "development",
-    base: "/",
-    srcDir: "src",
-    filename: "sw.ts",
-    includeAssets: ['favicon.ico', 'robots.txt', 'icons/apple-touch-icon.png'],
-    manifest: {
-      "theme_color": "#f69435",
-      "background_color": "#f69435",
-      "display": "fullscreen",
-      "scope": "/",
-      "start_url": "/",
-      "name": "Ankara Kendo Performance",
-      "short_name": "AnkaraKendo",
-      "description": "Ankara Kendo Board",
-      "icons": [
-        {
-          "src": "/icons/icon-192x192.png",
-          "sizes": "192x192",
-          "type": "image/png",
-          "purpose": "any maskable"
-        },
-        {
-          "src": "/icons/icon-256x256.png",
-          "sizes": "256x256",
-          "type": "image/png",
-          "purpose": "any maskable"
-        },
-        {
-          "src": "/icons/icon-384x384.png",
-          "sizes": "384x384",
-          "type": "image/png",
-          "purpose": "any maskable"
-        },
-        {
-          "src": "/icons/icon-512x512.png",
-          "sizes": "512x512",
-          "type": "image/png",
-          "purpose": "any maskable"
-        }
-      ]
-    },
-    devOptions: {
-      enabled: true
-      /* other options */
+  mode: "development",
+  plugins: [svelte()],
+  build: {
+    outDir: '/app/static/dist',
+    emptyOutDir:true,
+    sourcemap: true,
+    rollupOptions: {
+      input: pages,
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]'
+      }
     }
-  })]
-})
+  }
+});
